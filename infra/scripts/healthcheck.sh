e500dba5e5de44e5b94d1d81f77a637ad6089b60
Krish-Aggarwal <83492168+Krish-Aggarwal@users.noreply.github.com>
2026-06-30 13:44:34 +0530

    Update healthcheck.sh

--- infra/scripts/healthcheck.sh
+++ infra/scripts/healthcheck.sh
@@ -6,14 +6,13 @@ echo "======================================="
 echo " PostgreSQL Cluster Health Check"
 echo "======================================="
 
-# dynamically resolve paths from Jenkins workspace
-BASE_DIR="${WORKSPACE:-$(pwd)}"
-INVENTORY="${BASE_DIR}/infra/ansible/inventory.ini"
+BASE_DIR="$(pwd)"
+INVENTORY="$BASE_DIR/ansible/inventory.ini"
 
 echo "Using inventory: $INVENTORY"
 
 echo ""
-echo "Checking PostgreSQL service status..."
+echo "Checking service status..."
 echo "--------------------------------------"
 
 for node in primary sync_standby async_standby
@@ -28,9 +27,8 @@ do
         -a "systemctl is-active postgresql@17-main"
 done
 
-
 echo ""
-echo "Checking replication status on primary..."
+echo "Checking replication..."
 echo "--------------------------------------"
 
 ansible primary \
@@ -39,6 +37,5 @@ ansible primary \
     -m shell \
     -a "sudo -u postgres psql -c \"SELECT client_addr, state, sync_state FROM pg_stat_replication;\""
 
-
 echo ""
-echo "Health check completed successfully"
+echo "Health check completed"
